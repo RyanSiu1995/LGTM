89d64f8fa6ab02370a9706c24ba05fa9f82150be
Ryan SIU <ryan.siu@cloverhealth.com>
2020-08-21 13:42:17 +0800

    Implement the logic of auto-inject comments

---
+++ inject.js
@@ -0,0 +1,23 @@
+// Handle on Change function for approveRadio button
+approveRadio = Array.from(document.getElementsByName("pull_request_review[event]")).filter((elem) => {
+  return elem.value === "approve"
+})[0];
+approveRadio.onchange = (checkbox) => {
+  textarea = document.getElementsByName("pull_request_review[body]")[0]
+  if (textarea.textLength === 0) {
+    textarea.value = "LGTM"
+  }
+}
+
+// Handle on uncheck function for approveRadio button
+uncheckLGTM = (checkbox) => {
+  textarea = document.getElementsByName("pull_request_review[body]")[0]
+  if (textarea.value === "LGTM") {
+    textarea.value = ""
+  }
+}
+Array.from(document.getElementsByName("pull_request_review[event]")).filter((elem) => {
+  return elem.value !== "approve"
+}).forEach((elem) => {
+  elem.onchange = uncheckLGTM
+});
