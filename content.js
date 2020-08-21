8917b432b8383c26291f7262b56768af585471fe
Ryan SIU <ryan.siu@cloverhealth.com>
2020-08-21 15:07:58 +0800

    Fix the bug in injecting the function

--- content.js
+++ content.js
@@ -1,25 +1,38 @@
-console.debug("Running the injection script")
-// Handle on Change function for approveRadio button
-approveRadio = Array.from(document.getElementsByName("pull_request_review[event]")).filter((elem) => {
-  return elem.value === "approve"
-})[0];
-approveRadio.onchange = (checkbox) => {
-  textarea = document.getElementsByName("pull_request_review[body]")[0]
-  if (textarea.textLength === 0) {
-    textarea.value = "LGTM"
+console.log("Running the injection script")
+function inject() {
+  // Handle on Change function for approveRadio button
+  approveRadio = Array.from(document.getElementsByName("pull_request_review[event]")).filter((elem) => {
+    return elem.value === "approve"
+  })[0];
+  if (!approveRadio) {
+    setTimeout(inject, 500)
+    return
   }
-}
-
-// Handle on uncheck function for approveRadio button
-uncheckLGTM = (checkbox) => {
-  textarea = document.getElementsByName("pull_request_review[body]")[0]
-  if (textarea.value === "LGTM") {
-    textarea.value = ""
+  approveRadio.onchange = (checkbox) => {
+    textarea = document.getElementsByName("pull_request_review[body]")[0]
+    if (textarea.textLength === 0) {
+      textarea.value = "LGTM"
+    }
   }
+  
+  // Handle on uncheck function for approveRadio button
+  uncheckLGTM = (checkbox) => {
+    textarea = document.getElementsByName("pull_request_review[body]")[0]
+    if (textarea.value === "LGTM") {
+      textarea.value = ""
+    }
+  }
+  Array.from(document.getElementsByName("pull_request_review[event]")).filter((elem) => {
+    return elem.value !== "approve"
+  }).forEach((elem) => {
+    elem.onchange = uncheckLGTM
+  });
 }
-Array.from(document.getElementsByName("pull_request_review[event]")).filter((elem) => {
-  return elem.value !== "approve"
-}).forEach((elem) => {
-  elem.onchange = uncheckLGTM
-});
-console.debug("Finished the injection script")
+let re = new RegExp('https://github\.com/.*/.*/pull/.*/files')
+if (re.exec(window.location.href)) {
+  inject()
+}
+fileChanges = Array.from(document.getElementsByClassName("tabnav-tab js-pjax-history-navigate"))
+  .filter((elem) => new RegExp('https://github\.com/.*/.*/pull/.*/files').exec(elem.href))[0]
+fileChanges.onclick = inject
+console.log("Finished the injection script")
